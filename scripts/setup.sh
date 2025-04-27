ca5b01d1cc692eed137ed4894f146e7034e0a044
Robert <robertkunakorn@gmail.com>
2025-04-27 12:26:33 +0700

    Fix: setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,25 +8,26 @@ if [ -f ./.env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-function dbdoc() {
+function build() {
+    yarn
+    yarn test:mutation
+    _dbdoc
+}
+
+function merge() {
+    yarn
+    yarn lint
+    yarn tsc --noEmit
+    yarn test:mutation
+}
+
+function _dbdoc() {
     if [ -z "$DBDOCS_TOKEN" ]; then
         echo "Error: DBDOCS_TOKEN is not set."
         exit 1
     fi
-    # yarn global add dbdocs
+    # yarn add -D dbdocs
     # DBDOCS_TOKEN="$DBDOCS_TOKEN" dbdocs build ./schema.dbml --project nestjs
-}
-
-
-function build() {
-    yarn test:mutation
-    dbdoc
-}
-
-function merge() {
-    yarn lint
-    yarn tsc --noEmit
-    yarn test:mutation
 }
 
 
