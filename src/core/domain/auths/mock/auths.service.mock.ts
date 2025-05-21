242ae13cd39be680f9071e13282fe011fa1866cc
Robert <robertkunakorn@gmail.com>
2025-05-21 21:07:00 +0700

    Chore: remove mock

--- src/core/domain/auths/mock/auths.service.mock.ts
+++
@@ -1,7 +0,0 @@
-import { AuthsService } from '../auths.service';
-
-export class AuthsServiceMockFactory {
-  static generateToken(): ReturnType<AuthsService['generateToken']> {
-    return 'token';
-  }
-}
