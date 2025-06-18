55e42fc0e95f9085d4a2d204f9f6168a29f067a0
Robert <robertkunakorn@gmail.com>
2025-06-18 18:42:38 +0700

    Fix: zod pipe

--- src/core/middleware/validation/zod-validation.pipe.ts
+++ src/core/middleware/validation/zod-validation.pipe.ts
@@ -3,12 +3,31 @@ import { ZodError } from 'zod';
 
 import { ApiException } from '@core/shared/http/http.exception';
 
+function setNestedKey(
+  target: Record<string, any>,
+  path: (string | number)[],
+  message: string,
+): void {
+  let curr = target;
+
+  for (let i = 0; i < path.length; i++) {
+    const key = path[i];
+
+    if (i === path.length - 1) {
+      curr[key] = [message];
+    } else {
+      curr[key] ||= {};
+      curr = curr[key];
+    }
+  }
+}
+
 export const CoreZodValidationPipe = createZodValidationPipe({
   // provide custom validation exception factory
   createValidationException: (error: ZodError) => {
     const fields = {};
-    for (const [k, v] of Object.entries(error.flatten().fieldErrors)) {
-      fields[k] = v?.map((data) => data.toLowerCase()) || [];
+    for (const err of error.errors) {
+      setNestedKey(fields, err.path, err.message);
     }
 
     return new ApiException(
